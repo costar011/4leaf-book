7141ea6c862c2fd537b80cd361f28b2391da2a82
YERIM <dnaer456@gmail.com>
2020-12-17 20:54:29 +0900

    first

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+
+const PORT = 7777;
+
+const app = express();
+
+app.use(morgan(`dev`));
+
+app.use(express.static(path.join(__dirname, `/assets`)));
+
+app.set("view engine", "pug");
+
+app.listen(PORT, () => {
+  console.log(` ✅ ${PORT} Server Start`);
+});
